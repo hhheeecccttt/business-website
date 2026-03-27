e28411aec9dc3365e491a77c4d57916cc2042d4a
hhheeecccttt <hectorwang41@gmail.com>
2026-03-27 11:00:59 -0400

    Loading animation

--- script.js
+++ script.js
@@ -12,4 +12,23 @@ document.addEventListener('DOMContentLoaded', () => {
             }, 200);
         });
     });
+
+    let dotCount = 1;
+    const dotElement = document.querySelector(".dots");
+    function pulseDots() {
+        dotCount = (dotCount % 3) + 1;
+        dotElement.textContent = " .".repeat(dotCount);
+    }
+    
+    setInterval(pulseDots, 500);
+    
+    const loaderContainer = document.querySelector('.loader-container');
+    const deals = document.querySelector('.deals');
+    const feature = document.querySelector('.feature');
+
+    setTimeout(() => {
+        loaderContainer.style.display = 'none';
+        deals.style.display = 'block';
+        feature.style.display = 'block';
+    }, Math.floor(Math.random() * (4500 - 2000 + 1)) + 2000);
 });
